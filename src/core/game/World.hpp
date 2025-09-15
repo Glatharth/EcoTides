00ccfbf2c439c8b3c95135eb29809928e7bec63b
Thiago <minhamain3@gmail.com>
2025-09-15 13:47:15 -0300

    arrumei os includes, confere se está tudo certo

--- src/core/game/World.hpp
+++ src/core/game/World.hpp
@@ -1,8 +1,8 @@
 // World.hpp
 #ifndef ECOTIDES_WORLD_HPP
 #define ECOTIDES_WORLD_HPP
-#include "../../ui/card/Card.hpp"
-#include "../../ui/animation/Animation.hpp"
+#include "ui/card/Card.hpp"
+#include "ui/animation/Animation.hpp"
 
 class World {
 public:
