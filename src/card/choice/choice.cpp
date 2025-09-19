daeec47d6bdbde0eb361474253a6c3138159ee75
Thiago <minhamain3@gmail.com>
2025-09-19 20:26:12 -0300

    comitado

--- src/card/choice/choice.cpp
+++ src/card/choice/choice.cpp
@@ -1,1 +1,11 @@
-#include <choice.hpp>
+#include "choice.hpp"
+
+int Choice::computeChange(Decision decision, int baseValue, bool isLargeChange) {
+    int changeAmount = isLargeChange ? largeChange : smallChange;
+    if (decision == Decision::Yes) {
+        return baseValue + changeAmount;
+    }
+    else {
+        return baseValue - changeAmount;
+    }
+}
--- src/card/choice/choice.hpp
+++ src/card/choice/choice.hpp
@@ -1,1 +1,17 @@
-#pragma once
+#ifndef ECOTIDES_CHOICE_HPP
+#define ECOTIDES_CHOICE_HPP
+
+enum class Decision {
+    No,
+    Yes
+};
+
+class Choice {
+public:
+    static constexpr int smallChange = 1;
+    static constexpr int largeChange = 2;
+
+    static int computeChange(Decision decision, int baseValue, bool isLargeChange);
+};
+
+#endif
