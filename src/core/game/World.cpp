337cca7dc6eb0782a25c72a1cb3136b714bcab37
Thiago <minhamain3@gmail.com>
2025-09-19 19:16:21 -0300

    integrei telas e cartas, por enquanto nao tem o exit.

--- src/core/game/World.cpp
+++ src/core/game/World.cpp
@@ -1,70 +1,101 @@
 #include "World.hpp"
-#include <raylib-cpp.hpp>
 
 
-World::World() : dummy(0) {
+World* globalWorldInstance = nullptr;
+
+void startGameWrapper() {
+    if (globalWorldInstance) {
+        globalWorldInstance->startGame();
+    }
+}
+
+World::World() : card(nullptr), animation(nullptr), cardIndex(0), playerWon(false), playerLost(false) {
+    globalWorldInstance = this;
+
     screen = new Screens();
-    
+    screen->change(ScreenState::MENU);
+    screen->setStartGameCallback(startGameWrapper);
+}
 
+World::~World() {
+    if (animation) delete animation;
+    if (card) delete card;
+    delete screen;
+}
+
+void World::startGame() {
     cardIds[0] = 1;
     cardIds[1] = 2;
     cardIds[2] = 3;
     cardIds[3] = 4;
 
-    srand(static_cast<unsigned int>(time(nullptr)));
-
-    for (int i = 0; i < 4; ++i) {
-        int randIndex = rand() % 4;
-        int temp = cardIds[i];
-        cardIds[i] = cardIds[randIndex];
-        cardIds[randIndex] = temp;
-    }
-
     card = new Card(cardIds[0]);
     animation = new Animation(card);
     cardIndex = 1;
+
+    playerWon = false;
+    playerLost = false;
+
+    screen->change(ScreenState::GAME);
 }
-
-World::~World() {
-    delete animation;
-    delete card;
-
-    delete screen;
-}
-
-/**
- * @brief Reads user input and updates the state of the game.
- */
 
 void World::cardSwap() {
-    delete card;
-
-    card = new Card(cardIds[cardIndex]);  // cardIds e cardIndex são membros da classe
+    if (card) delete card;
+    card = new Card(cardIds[cardIndex]);
     cardIndex = (cardIndex + 1) % 4;
-
     animation->setCard(card);
 }
 
-
-
 void World::update(float delta) {
-    animation->update(delta);
-    if (animation->needsCardSwap()) {
-        cardSwap();
-        animation->resetSwap();
+    if (animation) {
+        animation->update(delta);
+
+        if (animation->needsCardSwap()) {
+            cardSwap();
+            animation->resetSwap();
+        }
+        /* IMPLEMENTAR CONDICOES DE VITÓRIA/DERROTA
+        if ((condicao de vitoria)false) {
+            playerWon = true;
+            screen->change(ScreenState::VICTORY);
+        }
+        else if ((condição de derrota)false) {
+            playerLost = true;
+            screen->change(ScreenState::DEFEAT);
+        }
+        */
+        //condicoes de vitória/derrota temporárias para teste
+        if (playerWon) {
+            // Garante que só mude uma vez para a tela de vitória
+            if (screen->getCurrentScreen() != ScreenState::VICTORY) {
+                screen->change(ScreenState::VICTORY);
+            }
+        }
+        else if (playerLost) {
+            if (screen->getCurrentScreen() != ScreenState::DEFEAT) {
+                screen->change(ScreenState::DEFEAT);
+            }
+        }
+        else {
+            if (screen->getCurrentScreen() != ScreenState::GAME) {
+                screen->change(ScreenState::GAME);
+            }
+        }
     }
+
     screen->update(delta);
 }
 
-
-/**
- * @brief Draws the state of the game.
- */
 void World::draw() {
     BeginDrawing();
     ClearBackground(RAYWHITE);
+
     screen->render();
-    animation->draw();
+
+    if (animation) {
+        animation->draw();
+    }
+
     DrawFPS(20, 20);
     EndDrawing();
 }
--- src/core/game/World.hpp
+++ src/core/game/World.hpp
@@ -1,32 +1,34 @@
 // World.hpp
 #ifndef ECOTIDES_WORLD_HPP
 #define ECOTIDES_WORLD_HPP
+#include <raylib-cpp.hpp>
 #include "ui/card/Card.hpp"
 #include "ui/animation/Animation.hpp"
-
-#pragma once
-
-#include <raylib-cpp.hpp>
 #include "utils/enum.hpp"
 #include "ui/screen/Screen.hpp"
 
-
+#pragma once
 
 class World {
 public:
     World();
     ~World();
-    void cardSwap();
+
+    void startGame(); 
     void update(float delta);
     void draw();
 
 private:
+    Screens* screen;
     Card* card;
     Animation* animation;
-    int dummy;
-    int cardIndex = 0;
+
     int cardIds[4];
-    Screens* screen;
+    int cardIndex;
+
+    bool playerWon;
+    bool playerLost;
+    void cardSwap();
 };
 
-#endif // ECOTIDES_WORLD_HPP
+#endif
